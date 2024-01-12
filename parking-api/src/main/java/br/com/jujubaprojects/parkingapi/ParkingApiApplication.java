afc1615394baa1211ae2bc863a6be3f7bd7d8d5a
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-12 10:02:56 -0300

    start the project

---
+++ parking-api/src/main/java/br/com/jujubaprojects/parkingapi/ParkingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jujubaprojects.parkingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApiApplication.class, args);
+	}
+
+}
---
+++ parking-api/src/test/java/br/com/jujubaprojects/parkingapi/ParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jujubaprojects.parkingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
