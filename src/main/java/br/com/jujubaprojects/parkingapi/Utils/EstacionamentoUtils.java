433dabf0d73a4d672673ebfe536227ee29d235c9
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-13 15:54:09 -0300

    added mapper and Utils, now gonna add repository and service with the buisness rule

---
+++ src/main/java/br/com/jujubaprojects/parkingapi/Utils/EstacionamentoUtils.java
@@ -0,0 +1,22 @@
+package br.com.jujubaprojects.parkingapi.Utils;
+
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+    //2023-03-16T15:23:48.616463500
+    // 20230316-152121
+    
+    private static String gerarRecibo(){
+        LocalDateTime date = LocalDateTime.now();
+        String recibo = date.toString().substring(0,19);
+        return recibo.replace("-", "")
+           .replace(":", "")
+           .replace("T", "-");
+
+    }
+}
---
+++ src/main/java/br/com/jujubaprojects/parkingapi/Web/exception/ClienteVagaMapper.java
@@ -0,0 +1,22 @@
+package br.com.jujubaprojects.parkingapi.Web.exception;
+
+import org.modelmapper.ModelMapper;
+
+import br.com.jujubaprojects.parkingapi.Entity.ClienteVaga;
+import br.com.jujubaprojects.parkingapi.Web.dto.EstacionamentoCreateDto;
+import br.com.jujubaprojects.parkingapi.Web.dto.EstacionamentoResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClienteVagaMapper {
+    
+    public static ClienteVaga toClienteVaga(EstacionamentoCreateDto dto) {
+        return new ModelMapper().map(dto, ClienteVaga.class);
+    }
+
+    public static EstacionamentoResponseDto toDto(ClienteVaga clienteVaga) {
+        return new ModelMapper().map(clienteVaga, EstacionamentoResponseDto.class);
+    }
+
+}
